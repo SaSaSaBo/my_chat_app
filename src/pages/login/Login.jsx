12865fdc326b9163638c1808697cd49748a88551
SaSaSaBo <sasasabo14@gmail.com>
2024-08-22 09:42:33 +0300

    Login.jsx ok?

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import skz from '../../assets/skz.png';
 
 const Login = () => {
+
+  const [currState, setCurrState] = useState("Sign Up");
+
   return (
     <div className='login'>
       <img src={skz} alt="" className='logo' />
       <form className='login-form'>
-        <h2>Sign Up</h2>
+        <h2>{currState}</h2>
         <input type='text' placeholder='Username' className='form-input' required/>
-        <input type='email' placeholder='Email' className='form-input' required/>
+        {
+          currState === "Sign Up" ? 
+          <input type='email' placeholder='Email' className='form-input' required/> :
+          null
+        }
         <input type='password' placeholder='Password' className='form-input' required/>
-        <button type='submit' className='form-button'>Sign Up</button>
+        <button type='submit' className='form-button'>
+          {
+            currState === "Sign Up" ? 
+            "Create Account" : 
+            "Login"
+          }
+        </button>
         <div className='login-term'>
           <input type='checkbox' /> 
           <p>Agree to the terms of use & privacy policy.</p>
         </div>
         <div className='login-forgot'>
-          <p className='login-toggle'>Already have an account? <span>Click here!</span></p>
+          {
+            currState === "Sign Up"?
+          <p className='login-toggle'>Already have an account? <span onClick={() => setCurrState("Login")}>Login now!</span></p> :
+          <p className='login-toggle'>Don't you have an account? <span onClick={() => setCurrState("Sign Up")}>Create one!</span></p>                   
+          }
+
+
         </div>
       </form>
     </div>
